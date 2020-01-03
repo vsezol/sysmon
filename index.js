ddbfb04a96eb650bb6a86aee4a054571554c9769
vsezol <vsevolod.zolotov02@mail.ru>
2020-01-03 17:54:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const chalk = require('chalk')
+
+const wsic = require('./modules/wsic')
+
+const INTRO = [
+	'                                           ',
+	'                                           ',
+	' _                                       _ ',
+	'| |                                     | |',
+	'| |__  _   _    __   _____  ___ _______ | |',
+	"| '_ \\| | | |   \\ \\ / / __|/ _ \\_  / _ \\| |",
+	'| |_) | |_| |    \\ V /\\__ \\  __// / (_) | |',
+	'|_.__/ \\__, |     \\_/ |___/\\___/___\\___/|_|',
+	'        __/ |                              ',
+	'       |___/                               ',
+	'                                           ',
+	'                                           ',
+	'                                           '
+]
+
+console.log(chalk.white.bold(INTRO.join('\n')))
+
+const arguments = process.argv
+
+if (arguments.length <= 2) {
+	console.log(
+		chalk.red(
+			'You need to run this app with arguments.\n' +
+				"Run this app '--help' for more info"
+		)
+	)
+} else if (arguments[2] === '--help') {
+	console.log(
+		chalk.green.bold('Arguments:\n') +
+			chalk.blue(
+				'    --update X     --    Update mode, where X is the time in ms.\n'
+			) +
+			chalk.blue('    --no-update    --    Without updating.')
+	)
+} else if (arguments[2] === '--no-update') {
+	if (arguments[3]) {
+		console.log(
+			chalk.red('More arguments than necessary.\n') +
+				chalk.red('Please run this app using --help')
+		)
+	} else {
+		setTimeout(() => console.log('\x1Bc'), 1000)
+		setTimeout(() => wsic(false, undefined), 1001)
+	}
+} else if (arguments[2] === '--update') {
+	if (isNaN(arguments[3])) {
+		console.log(
+			chalk.red('Delay is not a number.\n') +
+				chalk.red(
+					'Please enter the number or run this app using --help.'
+				)
+		)
+	} else {
+		setTimeout(() => console.log('\x1Bc'), 1000)
+		setTimeout(() => wsic(true, parseInt(arguments[3])), 1001)
+	}
+}
---
+++ modules/wsic.js
@@ -0,0 +1,124 @@
+const os = require('os'),
+	fs = require('fs'),
+	chalk = require('chalk')
+
+const getSysInfo = () => {
+	const cpu = os.cpus()
+	const cpu_model = os.cpus()[0].model + ''
+	const upt = os.uptime()
+	const hs = parseInt(upt / 3600)
+	const ms = parseInt((upt - hs * 3600) / 60)
+	const sc = upt - hs * 3600 - ms * 60
+	return {
+		static: {
+			platform: os.platform(),
+			architechture: os.arch(),
+			processor: {
+				model: cpu_model
+					.split(' ')
+					.slice(0, -4)
+					.join(' '),
+				frequency: cpu_model
+					.split(' ')
+					.slice(-1)
+					.join(),
+				cores: 'x' + cpu.length
+			},
+			'Total Memory': parseInt(os.totalmem() / Math.pow(1024, 2)) + ' Mb'
+		},
+		dinamic: {
+			'Free Memory': parseInt(os.freemem() / Math.pow(1024, 2)) + ' Mb',
+			'Up Time': `${hs} h ${ms} m ${sc} s`
+		}
+	}
+}
+
+const printSysInfo = info => {
+	console.log('\n' + chalk.green.underline.bold('Static:') + '\n')
+	console.log(
+		'    ' +
+		chalk.bgGreen.black(`Platform:`) +
+		' ' +
+		chalk.green(`${info.static.platform}`)
+	)
+	console.log(
+		'    ' +
+		chalk.bgGreen.black(`Architechture:`) +
+		' ' +
+		chalk.green(`${info.static.architechture}`)
+	)
+	console.log('    ' + chalk.bgGreen.black(`Processor:`))
+	console.log(
+		'        ' +
+		chalk.bgRed.black(`Model:`) +
+		' ' +
+		chalk.green(`${info.static.processor.model}`)
+	)
+	console.log(
+		'        ' +
+		chalk.bgRed.black(`Frequency:`) +
+		' ' +
+		chalk.green(`${info.static.processor.frequency}`)
+	)
+	console.log(
+		'        ' +
+		chalk.bgRed.black(`Cores:`) +
+		' ' +
+		chalk.green(`${info.static.processor.cores}`)
+	)
+	console.log(
+		'    ' +
+		chalk.bgGreen.black(`Total Memory:`) +
+		' ' +
+		chalk.green(`${info.static['Total Memory']}`)
+	)
+	console.log(
+		'\n' +
+		chalk.green.bold('---------------') +
+		chalk.blue.bold('---------------')
+	)
+	console.log('\n' + chalk.blue.underline.bold('Dinamic:') + '\n')
+	console.log(
+		'    ' +
+		chalk.bgBlue.black(`Free Memory:`) +
+		' ' +
+		chalk.blue(`${info.dinamic['Free Memory']}`)
+	)
+	console.log(
+		'    ' +
+		chalk.bgBlue.black(`Up Time:`) +
+		' ' +
+		chalk.blue(`${info.dinamic['Up Time']}`)
+	)
+}
+
+const writeSysInfoInConsole = (isUp, delay) => {
+	const getPromise = new Promise((res, rej) => {
+		res(getSysInfo())
+	})
+		.then(info => {
+			return new Promise((res, rej) => {
+				res(printSysInfo(info))
+			})
+		})
+		.then(() => {
+			return new Promise((res, rej) => {
+				if (isUp) {
+					setTimeout(() => {
+						console.log('\x1Bc')
+						res()
+					}, delay)
+				} else {
+					rej()
+				}
+			})
+		})
+		.then(() => {
+			writeSysInfoInConsole(isUp, delay)
+		})
+		.catch(err => {
+			console.log()
+		})
+}
+
+module.exports = writeSysInfoInConsole
